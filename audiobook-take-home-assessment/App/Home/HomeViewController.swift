e5ce62d72bbf1b14110ee0b11d07d574984d3d76
barif-7 <basilarif98@gmail.com>
2025-11-26 15:15:45 -0500

    podcast-list-feature/update HomeViewController's initialization

--- audiobook-take-home-assessment/App/Home/HomeViewController.swift
+++ audiobook-take-home-assessment/App/Home/HomeViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 final class HomeViewController: ViewController {
     // TODO: - Add feature dependencies here...
-    init() {
+    
+    private let rootFeature: RootFeature
+    private var embeddedViewController: UIViewController?
+
+    init(rootFeature: RootFeature) {
+        self.rootFeature = rootFeature
         super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let child = rootFeature.make()
+        embed(child)
+    }
+
+    // MARK: - Embedding
+    private func embed(_ child: UIViewController) {
+        if let current = embeddedViewController {
+            current.willMove(toParent: nil)
+            current.view.removeFromSuperview()
+            current.removeFromParent()
+        }
+
+        addChild(child)
+        view.addSubview(child.view)
+        
+        child.view.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            child.view.topAnchor.constraint(
+                equalTo: view.topAnchor)
+            ,
+            child.view.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor
+            ),
+            child.view.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor
+            ),
+            child.view.bottomAnchor.constraint(
+                equalTo: view.bottomAnchor
+            )
+        ])
+
+        child.didMove(toParent: self)
+        embeddedViewController = child
     }
 }
