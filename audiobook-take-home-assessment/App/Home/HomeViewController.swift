6e68ca05dea5dcda2d93920732ff23b4e3d84888
barif-7 <basilarif98@gmail.com>
2025-11-26 01:28:22 -0500

    initial setup
    • project structure
    • base configurations
    • scene delegate setup

---
+++ audiobook-take-home-assessment/App/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  audiobook-take-home-assessment
+//
+//  Created by Basil Arif on 2025-11-26.
+//
+
+import UIKit
+
+final class HomeViewController: ViewController {
+    // TODO: - Add feature dependencies here...
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ audiobook-take-home-assessment/Scene/SceneDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  SceneDelegate.swift
+//  audiobook-take-home-assessment
+//
+//  Created by Basil Arif on 2025-11-26.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        
+        // TODO: - Build any dependencies to inject as HomeViewController will be the single source of truth & the point of entry for any feature class.
+        let rootVC = HomeViewController()
+        window.rootViewController = rootVC
+        self.window = window
+        
+        window.makeKeyAndVisible()
+    }
+    
+    // MARK: - Conformances
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+
+    func sceneWillResignActive(_ scene: UIScene) {}
+
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+}
+
